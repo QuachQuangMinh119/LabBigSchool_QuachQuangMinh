225380f1af9bde004f20c52b5428b8d38cf9bad5
Asus <Asus@LAPTOP-PUV4NSCN>
2022-06-03 15:46:15 +0700

    Add project files.

---
+++ LabBigSchool_QuachQuangMinh/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabBigSchool_QuachQuangMinh.MvcApplication" Language="C#" %>
---
+++ LabBigSchool_QuachQuangMinh/ViewModels/ValidTimeAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace LabBigSchool_QuachQuangMinh.ViewModels
+{
+    internal class ValidTimeAttribute : Attribute
+    {
+    }
+}
