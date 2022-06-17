983d72692d9931014632d2b2b379133688684b23
Quang Minh Quách <quangminhcm2001@gmail.com>
2022-06-17 16:18:45 +0700

    update

---
+++ LabBigSchool_QuachQuangMinh/Migrations/202206170914388_UpdateDBs.cs
@@ -0,0 +1,16 @@
+namespace LabBigSchool_QuachQuangMinh.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateDBs : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- LabBigSchool_QuachQuangMinh/Models/ApplicationDbContext.cs
+++ LabBigSchool_QuachQuangMinh/Models/ApplicationDbContext.cs
@@ -11,6 +11,8 @@ namespace LabBigSchool_QuachQuangMinh.Models
     {
         public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<Following> Followings { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -20,5 +22,24 @@ namespace LabBigSchool_QuachQuangMinh.Models
         {
             return new ApplicationDbContext();
         }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            modelBuilder.Entity <Attendance> ()
+            .HasRequired(a => a.Course)
+            .WithMany()
+            .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followers)
+                .WithRequired(f => f.Followee)
+                .WillCascadeOnDelete(false);
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasMany(u => u.Followees)
+                .WithRequired(f => f.Follower)
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
